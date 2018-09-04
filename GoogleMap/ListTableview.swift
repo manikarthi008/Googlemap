eb0867a0d2f9a3b26164896606fce64ab25f73dc
Anil Kumar <anilkumar@Anils-iMac.local>
2018-09-04 16:11:06 +0530

    first commit

---
+++ GoogleMap/ListTableview.swift
@@ -0,0 +1,24 @@
+//
+//  ListTableview.swift
+//  GoogleMap
+//
+//  Created by Anil Kumar on 03/09/18.
+//  Copyright © 2018 Anil Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ListTableview: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
